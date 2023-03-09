3f69c18d3a6cb8730dd67d7eb8f4f3c553be9d51
ajw1079 <ajw1079@naver.com>
2023-03-09 18:58:06 +0900

    first Commit

---
+++ src/pages/common/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p className="text-center">Copyrights &copy; AdAge Ltd.</p>
+            </footer>
+        </>
+    )
+};
+export default Footer;
